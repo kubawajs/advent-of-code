c9f1417b6beea4a21f16633d0909d695cb90713b
kubawajs <jakub.wajs10@gmail.com>
2023-12-06 22:23:45 +0100

    Day 6 part 2

--- 2023/05/food-production-problem/src/main.rs
+++ 2023/05/food-production-problem/src/main.rs
@@ -3,14 +3,37 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    let file = File::open("../input.txt").unwrap();
-    let mut reader = io::BufReader::new(file);
+    if let Ok(lines) = read_lines("../input.txt") {
+        let mut seed_to_soil_map: Vec<Vec<usize>> = Vec::new();
+        let mut soil_to_fertilizer_map: Vec<Vec<usize>> = Vec::new();
+        let mut fertilizer_to_water_map: Vec<Vec<usize>> = Vec::new();
+        let mut water_to_light_map: Vec<Vec<usize>> = Vec::new();
+        let mut light_to_temperature_map: Vec<Vec<usize>> = Vec::new();
+        let mut temperature_to_humidity_map: Vec<Vec<usize>> = Vec::new();
+        let mut humidity_to_location_map: Vec<Vec<usize>> = Vec::new();
 
-    let mut line = String::new();
-    reader.read_line(&mut line).unwrap();
+        let file_content = lines.collect::<Vec<Result<String, io::Error>>>();
+        let seeds = read_seeds(file_content[0].as_ref().unwrap());
+        
+        let mut current_index: usize = 1;
+        while true {
+            let current_line = file_content[current_index].as_ref().unwrap();
+            current_index += 1;
+            if current_line.is_empty() == true {
+                break;
+            }
+            
+            if current_line.contains(":") {
+                continue;
+            }
 
-    let mut seeds: Vec<usize> = read_seeds(&line);
-    println!("Seeds: {:?}", seeds);
+            let map = current_line.split_whitespace().collect::<Vec<&usize>>();
+            seed_to_soil_map.push(map);
+        }
+
+        println!("Seed to soil map: {:?}", seed_to_soil_map);
+        println!("Seeds: {:?}", seeds);
+    }
 }
 
 fn read_seeds(ip: &str) -> Vec<usize> {
@@ -23,4 +46,14 @@ fn read_seeds(ip: &str) -> Vec<usize> {
     }
 
     return seeds;
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
