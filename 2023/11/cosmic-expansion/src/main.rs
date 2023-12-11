8d1e07ca00530045450365803940a3f5f633a637
kubawajs <jakub.wajs10@gmail.com>
2023-12-11 23:52:56 +0100

    Day 11 part 1 finished

--- 2023/11/cosmic-expansion/src/main.rs
+++ 2023/11/cosmic-expansion/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     // File input.txt must exist in the current path
     if let Ok(lines) = read_lines("../input.txt") {
         let mut galaxy_map = Vec::new();
-        let mut columns_with_galaxies = Vec::new();
+        let mut columns_with_galaxies: Vec<i32> = Vec::new();
         
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
@@ -18,8 +18,8 @@ fn main() {
                 }
                 else {
                     for (i, c) in row.iter().enumerate() {
-                        if *c == '#' && columns_with_galaxies.contains(&i) == false {
-                            columns_with_galaxies.push(i);
+                        if *c == '#' && columns_with_galaxies.contains(&(i as i32)) == false {
+                            columns_with_galaxies.push(i as i32);
                         }
                     }
                 }
@@ -35,11 +35,29 @@ fn main() {
 
         println!("{:?}", galaxies_coords);
 
-        for i in 0..galaxies_coords.len() - 1 {
-            let distance = (galaxies_coords[i + 1].0 - galaxies_coords[i].0) + (galaxies_coords[i + 1].1 - galaxies_coords[i].1);
-            println!("Distance: {}", distance);
+        let mut iterator = 0;
+        let mut overall_distance = 0;
+
+        loop {
+            for i in iterator..galaxies_coords.len() - 1 {
+                let distance = calculate_distance(galaxies_coords[iterator], galaxies_coords[i + 1]);
+                overall_distance += distance;
+                println!("Distance {:?}-{:?}: {}", galaxies_coords[iterator], galaxies_coords[i + 1], distance);
+            }
+
+            if iterator == galaxies_coords.len() - 1 {
+                break;
+            }
+            iterator += 1;
         }
+
+        println!("Overall distance: {}", overall_distance);
     }
+}
+
+fn calculate_distance (galaxy1: (i32, i32), galaxy2: (i32, i32)) -> i32 {
+    let distance = (galaxy1.0 - galaxy2.0).abs() + (galaxy1.1 - galaxy2.1).abs();
+    distance
 }
 
 fn get_galaxy_coords (galaxy_map: Vec<Vec<char>>) -> Vec<(i32, i32)> {
@@ -47,14 +65,14 @@ fn get_galaxy_coords (galaxy_map: Vec<Vec<char>>) -> Vec<(i32, i32)> {
     for (index, row) in galaxy_map.iter().enumerate() {
         for (i, c) in row.iter().enumerate() {
             if *c == '#' {
-                galaxies_coords.push((index, i));
+                galaxies_coords.push((index as i32, i as i32));
             }   
         }
     }
     galaxies_coords
 }
 
-fn adjust_galaxy_map (galaxy_map: Vec<Vec<char>>, columns_with_galaxies: &Vec<usize>) -> Vec<Vec<char>> {
+fn adjust_galaxy_map (galaxy_map: Vec<Vec<char>>, columns_with_galaxies: &Vec<i32>) -> Vec<Vec<char>> {
     let mut adjusted_galaxy_map: Vec<Vec<char>> = Vec::new();
     for old_row in galaxy_map {
         let mut new_row: Vec<char> = Vec::new();
@@ -62,7 +80,7 @@ fn adjust_galaxy_map (galaxy_map: Vec<Vec<char>>, columns_with_galaxies: &Vec<us
             new_row.push(cell.clone());
 
             // Duplicate the column if no galaxies are present
-            if columns_with_galaxies.contains(&index) == false {
+            if columns_with_galaxies.contains(&(index as i32)) == false {
                 new_row.push(cell.clone());
             }
         }
